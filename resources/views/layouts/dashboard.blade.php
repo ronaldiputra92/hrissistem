012fa20ad312a47327883bb7dc92167945a0c4ac
ronaldiputra92 <ronaldiputra92@gmail.com>
2025-06-20 20:29:31 +0700

    update baru

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -28,9 +28,10 @@
             <div class="sidebar-wrapper active">
                 <div class="sidebar-header position-relative">
                     <div class="d-flex justify-content-between align-items-center">
-                        <div class="logo">
+                        <h3 class="mt-3">Indocoll</h2>
+                        {{-- <div class="logo">
                             <a href="index.html"><img src="{{ asset('mazer/assets/compiled/svg/logo.svg') }}" alt="Logo" srcset=""></a>
-                        </div>
+                        </div> --}}
                         <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
                                 role="img" class="iconify iconify--system-uicons" width="20" height="20"
